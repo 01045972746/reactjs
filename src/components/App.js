bcebbe8fbcb3159596f424dfe40792bfb379de23
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-20 15:00:08 +0900

    [Component] Contact Selector (17.10.20)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import update from 'react-addons-update'
-import ContactCreator from './Contact/ContactCreator'
+import Contacts from './Contact/Contacts'
+
 
 
 class App extends React.Component {
@@ -11,55 +11,5 @@ class App extends React.Component {
     }
 }
 
-
-class Contacts extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            contactData: [
-                {name: "Abet", phone: "010-0000-0001"},
-                {name: "Betty", phone: "010-0000-0002"},
-                {name: "Charlie", phone: "010-0000-0003"},
-                {name: "David", phone: "010-0000-0004"}
-            ]
-        }
-    }
-
-    _insertContact(name, phone){
-        let newState = update(this.state, {
-            contactData: {
-                $push: [{"name": name, "phone": phone}]
-            }
-        });
-        this.setState(newState);
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Contacts</h1>
-                <ul>
-                    {this.state.contactData.map((contact, i) => {
-                        return ( <ContactInfo name={contact.name}
-                                            phone={contact.phone}
-                                              key={i}
-                            />);
-                    })}
-                </ul>
-                <ContactCreator onInsert={this._insertContact.bind(this)}/>
-            </div>
-        );
-    }
-}
-
-class ContactInfo extends React.Component {
-    render() {
-        return (
-            <li>{this.props.name} {this.props.phone}</li>
-        );
-    }
-}
 
 export default App;
---
+++ src/components/Contact/ContactInfo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ContactInfo extends React.Component {
+
+    handleClick() {
+        this.props.onSelect(this.props.contactKey);
+    }
+
+    render() {
+
+        let getStyle = isSelected => {
+            if(!isSelected) return;
+
+            let style = {
+                fontWeight: 'bold',
+                backgroundColor: '#4efcd8'
+            };
+
+            return style;
+        };
+
+
+
+        return (
+            <li style={getStyle(this.props.isSelected)}
+                onClick={this.handleClick.bind(this)}>
+                {this.props.name} {this.props.phone}
+            </li>
+        );
+    }
+}
+
+export default ContactInfo;
