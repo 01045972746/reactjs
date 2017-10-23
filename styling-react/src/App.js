92b704a7e5ce49e4d26f34ee77d436765bdf0f6b
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-23 15:33:27 +0900

    [CSSComponent] add yarn classnames

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import classNames from 'classnames';
 import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.box}>
+      <div className={classNames(styles.box, styles.blue)}>
       
       </div>
     );
