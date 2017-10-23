9ae90bc69aa2a9517af24f0bccc11a8512757c99
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-23 11:31:47 +0900

    [Router] redirect, dd-route example

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from './routes/Home'
 import About from './routes/About'
+import Posts from './routes/Posts'
+import Login from './routes/Login'
+import MyPage from './routes/MyPage'
 
 import Header from './components/Header'
+
 
 const App = () => {
   return (
@@ -12,7 +16,10 @@ const App = () => {
       <div>
         <Header/>
         <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
+        <Route path="/about/:username" component={About}/>
+        <Route path="/posts" component={Posts}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/mypage" component={MyPage}/>
       </div>
     </Router>
   );
--- router-app/src/routes/About.js
+++ router-app/src/routes/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const About = () => {
+const About = ({match}) => {
     return (
         <div>
-            소개 페이지
+            {match.params.username} 소개 페이지
         </div>
     );
 };
