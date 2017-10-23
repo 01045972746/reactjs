1ad16c5da6bc297071a13d8bebf17e97fb435869
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-23 17:54:37 +0900

    [CSSComponent] css to scss

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind';
-import styles from './App.css';
+import styles from './App.scss';
 const cx = classNames.bind(styles);
 
 class App extends Component {
