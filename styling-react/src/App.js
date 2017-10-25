e69f6f2a5b94ee36e6892b30a72cebb90b6fb2d7
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-25 10:00:30 +0900

    [CSSComponent] backquote js and big button

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Button from './Components/Button'
+import StyledButton from './Components/StyledButton'
 
 class App extends Component {
   render() {
-    const isBlue = true;
     return (
       <div>
-        <Button>버튼</Button>
+        <StyledButton big>버튼2</StyledButton>
       </div>
     );
   }
