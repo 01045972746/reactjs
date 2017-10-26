87d529b9b243e463e3a86142256345d790e7dc58
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-26 13:44:42 +0900

    [Django-React] edit to ES6

--- djangoreact/assets/js/app.jsx
+++ djangoreact/assets/js/app.jsx
@@ -1,7 +1,13 @@
-var React = require('react');
+import React from 'react'
 
-module.exports = React.createClass({
-   render: function(){
-       return <h1>Hello, world.</h1>
-   }
-});
+class App extends React.Component {
+    render() {
+        return (
+            <h1>HIHIHIH</h1>
+        );
+    }
+
+
+}
+
+export default App;
