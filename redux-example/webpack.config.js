59571d1f30ad44f833f16a6c2241725d1819f9ea
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-11-06 15:48:05 +0900

    redux re-start

--- redux-example/webpack.config.js
+++ redux-example/webpack.config.js
@@ -2,26 +2,25 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: __dirname + '/public',
+        path: __dirname + '/public/',
         filename: 'bundle.js'
     },
 
     devServer: {
         inline: true,
-        port: 8080,
-        contentBase: __dirname + '/public'
+        port: 7777,
+        contentBase: __dirname + '/public/'
     },
 
-    module:
-    {
+    module: {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
-                    presets: ['es2015', 'stage-0', 'react']
+                    presets: ['es2015', 'react']
                 }
             }
         ]
